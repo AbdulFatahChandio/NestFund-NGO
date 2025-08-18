be2050fde48176f4c197e3e9528d550d05ffe3ac
AbdulFatahChandio <abdulfatahchandio61@gmail.com>
2025-08-18 13:16:10 +0500

    role-permission seed updated to avoid duplication

--- prisma/Seed/role-permission.seed.ts
+++ prisma/Seed/role-permission.seed.ts
@@ -1,49 +1,80 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 export async function seedRolePermission() {
-    await prisma.rolePermission.upsert({
-        where: {
-            roleId_permissionId: { 
-                roleId: 1,
-                permissionId: 1
-            }
-        },
-        update: {}, 
-        create: {
+    const rolePerm = [
+        {
             roleId: 1,
-            permissionId: 1
-        }
-    });
-
-    await prisma.rolePermission.upsert({
-        where: {
-            roleId_permissionId: { 
-                roleId: 1,
-                permissionId: 2
-            }
+            permissionId: [1, 2, 3],
         },
-        update: {}, 
-        create: {
-            roleId: 1,
-            permissionId: 2
-        }
-    });
-
-    await prisma.rolePermission.upsert({
-        where: {
-            roleId_permissionId: { 
-                roleId: 1,
-                permissionId: 3
-            }
+        {
+            roleId: 2,
+            permissionId: [1, 3],
         },
-        update: {}, 
-        create: {
-            roleId: 1,
-            permissionId: 3
+    ];
+
+    for (const roleP of rolePerm) {
+        for (const perm of roleP.permissionId) {
+            await prisma.rolePermission.upsert({
+                where: {
+                    roleId_permissionId: {
+                        roleId: roleP.roleId,
+                        permissionId: perm,
+                    },
+                },
+                update: {}, // nothing to update if it exists
+                create: {
+                    roleId: roleP.roleId,
+                    permissionId: perm,
+                },
+            });
         }
-    });
-
-
-    console.log("Role Permission seeded executed :");
+    }
 }
+
+
+    // await prisma.rolePermission.upsert({
+    //     where: {
+    //         roleId_permissionId: { 
+    //             roleId: 1,
+    //             permissionId: 1
+    //         }
+    //     },
+    //     update: {}, 
+    //     create: {
+    //         roleId: 1,
+    //         permissionId: 1
+    //     }
+    // });
+
+    // await prisma.rolePermission.upsert({
+    //     where: {
+    //         roleId_permissionId: { 
+    //             roleId: 1,
+    //             permissionId: 2
+    //         }
+    //     },
+    //     update: {}, 
+    //     create: {
+    //         roleId: 1,
+    //         permissionId: 2
+    //     }
+    // });
+
+    // await prisma.rolePermission.upsert({
+    //     where: {
+    //         roleId_permissionId: { 
+    //             roleId: 1,
+    //             permissionId: 3
+    //         }
+    //     },
+    //     update: {}, 
+    //     create: {
+    //         roleId: 1,
+    //         permissionId: 3
+    //     }
+    // });
+
+
+    // console.log("Role Permission seeded executed :");
